1a1c968efd3d2217698f4ddf1cfe6e2e45deb507
Dorota Przeniosło <d.przenioslo@centrala.kaczmarski.pl>
2016-10-20 14:50:17 +0200

    slider sliding shit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-//impure as hell
+//impure as hell  
 var BaseComponent = {
     //helpers
     _loopOverProperties(object, callback) {
@@ -35,7 +35,7 @@ const Slide = React.createClass({
     _props: { source: "", className: "" },
     render() {
         return (
-            <img src={this._props.source} width="200" className={this._props.className} />
+            <img src={this._props.source} className={this._props.className} />
         );
     }
 
@@ -77,32 +77,64 @@ const Controls = React.createClass({
 
 function SliderWrapper(Component) {
     return React.createClass({
-        mixins: [BaseComponent],
+        //mixins: [BaseComponent],
         _state: { currentSlide: 0 },
         _props: { sources: [], children: {} },
         getInitialState() { return this._state },
-
+        moveRight: false,
+        moveLeft: false,
+        slides: [],
+        prev: 3,
+        next: -1,
+        componentWillMount() {
+            this.slides = this.props.sources.map(function (imgPath, index) {
+                //let className = "active-" + (currentSlide === index);
+                return <Slide source={imgPath} />;
+            });
+        },
         nextClicked(_) {
-            var newCurrentSlide = (this._state.currentSlide + 1) % this._props.sources.length;
-            this.setStateWH({ currentSlide: newCurrentSlide });
+           var newCurrentSlide = (this.state.currentSlide + 1) % this.props.sources.length;
+            this.moveLeft = false;
+            this.moveRight = true;
+            this.setState({ currentSlide: newCurrentSlide });
+
         },
         prevClicked(_) {
-            var newCurrentSlide = (this._props.sources.length + this._state.currentSlide - 1) % this._props.sources.length;
-            this.setStateWH({ currentSlide: newCurrentSlide });
+            var newCurrentSlide = (this.props.sources.length + this.state.currentSlide - 1) % this.props.sources.length;
+            this.moveRight = false;
+            this.moveLeft = true;
+            this.setState({ currentSlide: newCurrentSlide });
+        },
+        getPrev() {
+            return (this.prev - 1 < 0 ) ? this.prev = this.props.sources.length - 1 : this.prev = this.prev - 1;
+        },
+        getNext() {
+            return (this.next + 1 === this.props.sources.length ) ? this.next = 0 : this.next = this.next + 1;
         },
         render() {
-            const currentSlide = this._state.currentSlide,
-                slidesCount = this._props.sources.length;
-            const slides = this._props.sources.map(function (imgPath, index) {
-                let className = "active-" + (currentSlide === index);
-                return <Slide source={imgPath} className={className} />;
-            });
-
+            console.log(this.slides);
+            const currentSlide = this.state.currentSlide,
+                slidesCount = this.props.sources.length;
+            // const slides = this.props.sources.map(function (imgPath, index) {
+            //     //let className = "active-" + (currentSlide === index);
+            //     return <Slide source={imgPath} />;
+            // });
+            var aaa = [];
+            if (this.moveRight) {
+                let index = this.getNext();
+                console.log("przenosze na koniec slajd numer " + index);
+                this.slides = [...this.slides, <Slide source={this.props.sources[index]} />];
+            } else if (this.moveLeft) {
+                let index =this.getPrev();
+                console.log("przenosze na początek slajd numer " + index + "current slide to "+ currentSlide);
+                this.slides = [<Slide source={this.props.sources[index]} />, ...this.slides];
+            }
+            const leftPadding =0 ;// -1 * currentSlide * 200;
             return (
                 <div id="slider">
                     <button onClick={this.prevClicked}>Prev</button>
                     <button onClick={this.nextClicked}>Next</button>
-                    <div id="slides">{slides}</div>
+                    <div id="slides" style={{ left: leftPadding }}>{this.slides}</div>
                     <Component currentSlide={currentSlide} slidesCount={slidesCount} />
 
                 </div>
@@ -114,7 +146,7 @@ function SliderWrapper(Component) {
 const App = React.createClass({
     displayName: "App",
     render() {
-        const images = ["images/p1.jpg", "images/p2.jpg", "images/p3.jpg"];
+        const images = ["http://www.celebritynetworth123.com/wp-content/plugins/networthdisplay/cnwimages/s/sasha-grey.jpg", "images/p2.jpg", "http://www.royalcanin.ca/~/media/Royal-Canin-Canada/Product-Categories/cat-adult-landing-hero.ashx"];
         return (
             <div id="app">
                 <Slider1 sources={images} />
@@ -124,7 +156,7 @@ const App = React.createClass({
     }
 });
 
-var Slider1 = SliderWrapper(Controls2);
+var Slider1 = SliderWrapper(Controls);
 
 
 ReactDOM.render(<App />, document.getElementById("main"));
